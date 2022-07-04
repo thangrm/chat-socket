fe7738ca2f906b592f83846d26da479240accb4d
thanghv <thang.hoang@adamosoft.com>
2022-07-04 16:51:18 +0700

    chat public

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Message;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::middleware('auth')->group(function (){
+    Route::get('/chat', function() {
+        return view('chat');
+    });
+
+    Route::get('/getUserLogin', function() {
+        return Auth::user();
+    });
+
+    Route::get('/messages', function() {
+        return Message::with('user')->get();
+    });
+
+    Route::post('/messages', function() {
+        $user = Auth::user();
+
+        $message = new App\Models\Message();
+        $message->message = request()->get('message', '');
+        $message->user_id = $user->id;
+        $message->save();
+
+        broadcast(new App\Events\MessagePosted($message, $user))->toOthers();
+        return ['message' => $message->load('user')];
+    });
 });
